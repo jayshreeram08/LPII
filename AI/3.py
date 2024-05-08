df926ccce802db3c3658ce9012ce994172e38db7
jayshreeram08 <150379208+jayshreeram08@users.noreply.github.com>
2024-05-09 00:22:03 +0530

    Selection Sort

--- AI/3.py
+++ AI/3.py
@@ -1,44 +1,30 @@
-"""
-3.Implement Greedy search algorithm for any of the following application:
-Prim's Minimal Spanning Tree Algorithm
-"""
-import heapq
-class Graph:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[] for i in range(vertices)]
+# #Implement Greedy search algorithm for any of the following application:
+# Selection Sort
 
-    def add_edge(self, u, v, w):
-        self.graph[u].append((v, w))
-        self.graph[v].append((u, w))
-
-    def prim_mst(self):
-        pq = [(0, 0)]
-        mst_cost = 0
-        in_mst = [False] * self.V
-        result = []
-
-        while pq:
-            weight, u = heapq.heappop(pq)
-            if in_mst[u]:
-                continue
-
-            in_mst[u] = True
-            mst_cost += weight
-            result.append((u, weight))
-
-            for v, cost in self.graph[u]:
-                if not in_mst[v]:
-                    heapq.heappush(pq, (cost, v))
-
-        return result, mst_cost
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
         
-g = Graph(4)
-g.add_edge(0, 1, 10)
-g.add_edge(0, 2, 6)
-g.add_edge(0, 3, 5)
-g.add_edge(1, 3, 15)
-g.add_edge(2, 3, 4)
-result, mst_cost = g.prim_mst()
-print("Edges in MST with their weights:", result)
-print("Cost of MST:", mst_cost)
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        # Swap 
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+def main():
+    print("Enter the size")
+    n = int(input())
+    arr = []
+    
+    for i in range(n):
+        print("enter {} element".format(i))
+        no = int(input())
+        arr.append(no)
+        
+    sorted_arr = selection_sort(arr)
+    print("Sorted array:", sorted_arr)
+
+if __name__ == "__main__":
+    main()
